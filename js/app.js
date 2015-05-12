c3b9b8267cf1cc27d0541692b0d94918b984434a
anton-b117 <anton@maroonspear.com>
2015-05-12 01:01:03 -0400

    Refactering jQuery DRY.

--- js/app.js
+++ js/app.js
@@ -1,33 +1,56 @@
 var main = function () {
 	"use strict";
 
-	$(".tabs a:nth-child(1)").on("click", function () {
-		// make all tabs inactive
+	var makeTabActive = function (tabNumber) {
+		// Constuct the selector from the tabNumber
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
 		$(".tabs span").removeClass("active");
+		$("tabSelector").addClass("active");
+		$("main .content").empty();
+	}
 
-		// make the first tab active
-		$(".tabs a:nth-child(1) span").addClass("active");
-
-		// empty the main content so we can recreate it
-		$("main.content").empty();
-
-		// return false so we dont follow the link
+	$(".tabs a:nth-child(1)").on("click", function () {
+		makeTabActive(1);
 		return false;
 	});
 
-	$(".tabs a:nth-child(1)").on("click", function () {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(1) span").addClass("active");
-		$("main.content").empty();
+	$(".tabs a:nth-child(2)").on("click", function () {
+		makeTabActive(2);
 		return false;
 	});
 
-	$(".tabs a:nth-child(1)").on("click", function () {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(1) span").addClass("active");
-		$("main.content").empty();
+	$(".tabs a:nth-child(3)").on("click", function () {
+		makeTabActive(3);
 		return false;
 	});
+
+	// $(".tabs a:nth-child(1)").on("click", function () {
+	// 	// make all tabs inactive
+	// 	$(".tabs span").removeClass("active");
+
+	// 	// make the first tab active
+	// 	$(".tabs a:nth-child(1) span").addClass("active");
+
+	// 	// empty the main content so we can recreate it
+	// 	$("main.content").empty();
+
+	// 	// return false so we dont follow the link
+	// 	return false;
+	// });
+
+	// $(".tabs a:nth-child(1)").on("click", function () {
+	// 	$(".tabs span").removeClass("active");
+	// 	$(".tabs a:nth-child(1) span").addClass("active");
+	// 	$("main.content").empty();
+	// 	return false;
+	// });
+
+	// $(".tabs a:nth-child(1)").on("click", function () {
+	// 	$(".tabs span").removeClass("active");
+	// 	$(".tabs a:nth-child(1) span").addClass("active");
+	// 	$("main.content").empty();
+	// 	return false;
+	// });
 
 	console.log("Hello, Mordor.")
 }
