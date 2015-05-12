1673b2693b822c7ca2f7e61c4124d01d7f475a04
anton-b117 <anton@maroonspear.com>
2015-05-12 00:48:46 -0400

    Building tab interactivity.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,37 @@
 var main = function () {
 	"use strict";
 
+	$(".tabs a:nth-child(1)").on("click", function () {
+		// make all tabs inactive
+		$(".tabs span").removeClass("active");
+
+		// make the first tab active
+		$(".tabs a:nth-child(1) span").addClass("active");
+
+		// empty the main content so we can recreate it
+		$("main.content").empty();
+
+		// return false so we dont follow the link
+		return false;
+	});
+
+	$(".tabs a:nth-child(1)").on("click", function () {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(1) span").addClass("active");
+		$("main.content").empty();
+		return false;
+	});
+
+	$(".tabs a:nth-child(1)").on("click", function () {
+		$(".tabs span").removeClass("active");
+		$(".tabs a:nth-child(1) span").addClass("active");
+		$("main.content").empty();
+		return false;
+	});
+
 	console.log("Hello, Mordor.")
 }
+
+
 
 $(document).ready(main);
