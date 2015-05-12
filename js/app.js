7b454ab5a0fc03d56426b6d5307d0708634aefc1
anton-b117 <anton@maroonspear.com>
2015-05-12 01:11:55 -0400

    Refactored tab interactivity by way of for loop.

--- js/app.js
+++ js/app.js
@@ -1,54 +1,39 @@
 var main = function () {
 	"use strict";
 
-	var makeTabActive = function (tabNumber) {
-		// Constuct the selector from the tabNumber
+	var tabNumber;
+
+	for (tabNumber = 1; tabNumber <= 3; tabNumber++) {
 		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
-		$(".tabs span").removeClass("active");
-		$("tabSelector").addClass("active");
-		$("main .content").empty();
+		$(tabSelector).on("click", function (event) {
+			$(".tabs span").removeClass("active");
+			$(event.target).addClass("active");
+			return false;
+		});
 	}
 
-	$(".tabs a:nth-child(1)").on("click", function () {
-		makeTabActive(1);
-		return false;
-	});
 
-	$(".tabs a:nth-child(2)").on("click", function () {
-		makeTabActive(2);
-		return false;
-	});
 
-	$(".tabs a:nth-child(3)").on("click", function () {
-		makeTabActive(3);
-		return false;
-	});
+	// var makeTabActive = function (tabNumber) {
+	// 	// Constuct the selector from the tabNumber
+	// 	var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
+	// 	$(".tabs span").removeClass("active");
+	// 	$("tabSelector").addClass("active");
+	// 	$("main .content").empty();
+	// }
 
 	// $(".tabs a:nth-child(1)").on("click", function () {
-	// 	// make all tabs inactive
-	// 	$(".tabs span").removeClass("active");
-
-	// 	// make the first tab active
-	// 	$(".tabs a:nth-child(1) span").addClass("active");
-
-	// 	// empty the main content so we can recreate it
-	// 	$("main.content").empty();
-
-	// 	// return false so we dont follow the link
+	// 	makeTabActive(1);
 	// 	return false;
 	// });
 
-	// $(".tabs a:nth-child(1)").on("click", function () {
-	// 	$(".tabs span").removeClass("active");
-	// 	$(".tabs a:nth-child(1) span").addClass("active");
-	// 	$("main.content").empty();
+	// $(".tabs a:nth-child(2)").on("click", function () {
+	// 	makeTabActive(2);
 	// 	return false;
 	// });
 
-	// $(".tabs a:nth-child(1)").on("click", function () {
-	// 	$(".tabs span").removeClass("active");
-	// 	$(".tabs a:nth-child(1) span").addClass("active");
-	// 	$("main.content").empty();
+	// $(".tabs a:nth-child(3)").on("click", function () {
+	// 	makeTabActive(3);
 	// 	return false;
 	// });
 
