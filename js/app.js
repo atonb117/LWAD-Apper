fced253ced77def8a3ed5c817c61d1adb6502294
anton-b117 <anton@maroonspear.com>
2015-05-12 01:37:20 -0400

    Implemented simple console log test for each tab.

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 var main = function () {
 	"use strict";
 
-	$(".tabs span").toArray().forEach(function (element) {
+	$(".tabs a span").toArray().forEach(function (element) {
 		// create a click header for this element
 		$(element).on("click", function () {
-			$(".tabs span").removeClass("active");
-			$(element).addClass("active");
+			// Since we are using the jQuery version of element,
+			// we'll go ahead and create a temp variable
+			// so we wont need to keep recreating it.
+			var $element = $(element);
+
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
+
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("First tab... CLICKED!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Second tab... Click CLICKED!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("First tab... Click Click CLICKED!");
+			}
+
 			return false;
 		});
 	});
