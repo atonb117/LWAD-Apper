409b7bbb21a1b7665cbc276c95fb6944ba7d0f96
anton-b117 <anton@maroonspear.com>
2015-05-31 04:30:13 -0400

    Corrected commas masquerading as periods.

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ var main = function () {
 
 	console.log("Hello Mordor.");
 
-	$(".tabs a:nth-child(1)"),on("click", function () {
+	$(".tabs a:nth-child(1)").on("click", function () {
 		//  make all the tabs inactive
 		$(".tabs span").removeClass("active");
 
 		// make the first tab active
-		$(".tabs a:nth-child(1) span"),addClass("active");
+		$(".tabs a:nth-child(1) span").addClass("active");
 
 		// empty the main content so we can recreate it
 		$("main .content").empty();
@@ -17,16 +17,16 @@ var main = function () {
 		return false;
 	});
 
-	$(".tabs a:nth-child(2)"),on("click", function () {
+	$(".tabs a:nth-child(2)").on("click", function () {
 		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(2) span"),addClass("active");
+		$(".tabs a:nth-child(2) span").addClass("active");
 		$("main .content").empty();
 		return false;
 	});
 
-	$(".tabs a:nth-child(3)"),on("click", function () {
+	$(".tabs a:nth-child(3)").on("click", function () {
 		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(3) span"),addClass("active");
+		$(".tabs a:nth-child(3) span").addClass("active");
 		$("main .content").empty();
 		return false;
 	});
