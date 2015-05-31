b8a4934407a4d5af8ca569f01bf391418b79f7ba
anton-b117 <anton@maroonspear.com>
2015-05-31 04:41:43 -0400

    Refactored using function.

--- js/app.js
+++ js/app.js
@@ -3,31 +3,26 @@ var main = function () {
 
 	console.log("Hello Mordor.");
 
-	$(".tabs a:nth-child(1)").on("click", function () {
-		//  make all the tabs inactive
+	var makeTabActive = function (tabNumber) {
+		// construct the selector from the tabNumber
+		var tabSelector = ".tabs a:nth-child(" + tabNumber + ") span";
 		$(".tabs span").removeClass("active");
-
-		// make the first tab active
-		$(".tabs a:nth-child(1) span").addClass("active");
-
-		// empty the main content so we can recreate it
+		$(tabSelector).addClass("active");
 		$("main .content").empty();
+	};
 
-		// return false so we dont follow the link
+	$(".tabs a:nth-child(1)").on("click", function () {
+		makeTabActive(1);
 		return false;
 	});
 
 	$(".tabs a:nth-child(2)").on("click", function () {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(2) span").addClass("active");
-		$("main .content").empty();
+		makeTabActive(2);
 		return false;
 	});
 
 	$(".tabs a:nth-child(3)").on("click", function () {
-		$(".tabs span").removeClass("active");
-		$(".tabs a:nth-child(3) span").addClass("active");
-		$("main .content").empty();
+		makeTabActive(3);
 		return false;
 	});
 
