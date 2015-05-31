da41acec7efb9c9e2190e14f4c8097af0caf140c
anton-b117 <anton@maroonspear.com>
2015-05-31 05:17:46 -0400

    Refactored using jQuery parent Function.

--- js/app.js
+++ js/app.js
@@ -3,13 +3,27 @@ var main = function () {
 
 	console.log("Hello Mordor.");
 
-	$(".tabs span").toArray().forEach(function (element) {
+	$(".tabs a span").toArray().forEach(function (element) {
 		// create a click handler for this element
 		$(element).on("click", function () {
-			$(".tabs span").removeClass("active");
-			$(element).addClass("active");
+			// since we're using the jQuery version of element
+			// we'll go ahead and creat a temporary variable
+			// so we don't need to keep recreating it
+			var $element = $(element);
+
+			$(".tabs a span").removeClass("active");
+			$element.addClass("active");
 			$("main .content").empty();
-			return false
+
+			if ($element.parent().is(":nth-child(1)")) {
+				console.log("First clicky!");
+			} else if ($element.parent().is(":nth-child(2)")) {
+				console.log("Second clicky!");
+			} else if ($element.parent().is(":nth-child(3)")) {
+				console.log("Third clicky!");
+			}
+
+			return false;
 		});
 	});
 };
